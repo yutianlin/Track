ef6c97aa243f2b465e65a3a9f5a59278c34f6e98
Yutian Lin <yutianlin@wegalvanize.com>
2020-11-06 17:53:25 -0500

    COV-60 Get endpoints for notification

---
+++ server/src/notifications/notification.service.ts
@@ -0,0 +1,18 @@
+import QueryService from "../QueryService";
+import { GetAllNotifications, GetNotificationById } from "./notification.queries";
+
+export default class Faculty {
+  queryService: QueryService;
+
+  constructor() {
+    this.queryService = new QueryService();
+  }
+
+  getAllNotifications = async () => {
+    return this.queryService.query(GetAllNotifications);
+  };
+
+  getNotificationById = async (id: number) => {
+    return this.queryService.query(GetNotificationById(id));
+  };
+}
